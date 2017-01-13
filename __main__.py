ef08f61a5e688e543510e400d628ec914b40290a
Vojtech Kotek <kotek.vojtech@gmail.com>
2017-01-13 16:06:35 +0100

    Pepification and adding argparse module. Now uses random charset if not specified

--- __main__.py
+++ __main__.py
@@ -2,22 +2,40 @@
 # coding: utf-8
 
 from PIL import Image, ImageDraw, ImageFont
-import sys
+import random
+import argparse
 
 font_family = 'unifont-9.0.06.ttf'
 
 # Character sets you can use, just change value in defaults in main()
-symbs = '±<>!@#$%^&*/-. '
-kanji1 = "蜀輸雄明京手子汁り"
-kanji2 = '置蜀輸雄明京手子汁りしン'
-kanji3 = 'フォントサイズをにして、表示位置を少し調整すると、文字は表示できた。'
-kanji4 = '几仄仇什叩伜卍吃牝佇吠囮妓屁巫彷扮肛呟呵呻姐姑妾怯拗沽泄狗狐爬姜屍獰懃鰻鱈攣讐躙鰹'
-kanji5= '一右雨円王音下火花貝学気九休玉金空月犬見五口校左三山子四糸字耳七車手十出女小上森人水正生青夕石赤千川先早草足村大男竹中虫町天田土二日入年白八百文木本名目立力林六'
-korean = 'ㄱㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅇㅈㅊㅋㅌㅍㅎㄲㅆᇸ'
-korean2 = 'ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ '
+charsets = {
+    'symbols': [
+        '±<>!@#$%^&*/-. ', 0.5
+    ],
+    'kanji': [
+        '置蜀輸雄明京手子汁りしン', 1
+    ],
+    'kanji2': [
+        'フォントサイズをにして、表示位置を少し調整すると、文字は表示できた。', 1
+    ],
+    'kanji3': [
+        '几仄仇什叩卍吃牝吠妓屁巫彷扮肛呟呵呻姐姑妾怯拗沽泄狗狐姜屍獰懃鰻鱈攣躙鰹', 1
+    ],
+    'kanji4': [
+        '一右雨円花貝学空月犬見五口三山子四糸字人水正生青夕石赤千川先早草足村大', 1
+    ],
+    'hangul': [
+        'ㄱㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅇㅈㅊㅋㅌㅍㅎㄲㅆᇸ', 1
+    ],
+    'hangul2': [
+        'ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ ', 1
+    ],
+}
+
 
 class Characterize(object):
-    """Converts an image to ASCII, you can set the size and input your own characters for it to use.
+    """Converts an image to ASCII, you can set the size and input your \
+    own characters for it to use.
 
     USAGE:
 
@@ -25,7 +43,7 @@ class Characterize(object):
 
     options:
     [-s][-size]     width in characters (integer)
-    [-c][-charset]  Characters to be used (string) leave blank for default presets
+    [-c][-charset]  Characters to be used (string) leave blank for defaults
     [-r][-ratio]    width to height ratio"""
 
     def __init__(self):
@@ -34,17 +52,17 @@ class Characterize(object):
     # Main function that calls all others
     def img2chars(self, img, size, ratio, charset):
         charlist = self.chars2dict(charset)
-        im = Image.open(img,'r').convert('L')
+        im = Image.open(img, 'r').convert('L')
         resize = (size, int(size/(im.width/(im.height*ratio))))
         im = im.resize(resize)
         for h in range(im.height):
             for w in range(im.width):
-                x = im.getpixel(xy=(w,h))
-                print(self.getSymbol(x, charlist),end='')
+                x = im.getpixel(xy=(w, h))
+                print(self.getSymbol(x, charlist), end='')
             print('')
 
     # Goes through charset and creates a sorted dictionary of chars: brightness
-    def chars2dict(self,charset):
+    def chars2dict(self, charset):
         charvals = []
         chardict = {}
         for char in charset:
@@ -54,15 +72,15 @@ class Characterize(object):
         multiplier = 255/(max(charvals)-min(charvals))
         final = []
         for key in chardict:
-            final.append((key,int((chardict[key]-min(charvals))*multiplier)))
+            final.append((key, int((chardict[key]-min(charvals))*multiplier)))
         return sorted(final, key=lambda x: x[1])
 
     # Prints the character and analyzes & returns average brightness.
     def char2val(self, char):
         fnt = ImageFont.truetype(font_family, 30)
-        txt = Image.new('L', (40,40),(255))
+        txt = Image.new('L', (40, 40), (255))
         d = ImageDraw.Draw(txt)
-        d.text((1,1), char, font=fnt, fill=(0), spacing=0)
+        d.text((1, 1), char, font=fnt, fill=(0), spacing=0)
         value = list(txt.getdata())
         val = int(sum(value)/len(value))
         return val
@@ -77,26 +95,33 @@ class Characterize(object):
 
 def main():
 
-    # Default values
-    size = 50
-    ratio = 1
-    charset = kanji3
+    # Get radom charset and its ratio
+    def randomCharset():
+        rnd = random.randrange(len(charsets))
+        r = [charsets[i] for i in charsets][rnd]
+        return r
 
-    args = sys.argv[1:]
+    rc = randomCharset()
 
-    img = args[0] # Set first arg to img and remove it.
-    args = args[1:]
+    # Pass arguments to vars
+    parser = argparse.ArgumentParser(description='Image to characters.')
+    parser.add_argument('image', metavar='img', type=str, nargs='?',
+                        help='Path to image.')
+    parser.add_argument('-c', dest='charset', type=str, default=rc[0],
+                        help='String of characters to use')
+    parser.add_argument('-s', dest='size', type=int, default=50,
+                        help='Size in characters.')
+    parser.add_argument('-r', dest='ratio', type=float, default=rc[1],
+                        help='Ratio of characters. ')
+    args = parser.parse_args()
 
-    # Get and asisgn all args
-    for n in range( len(args) ):
-        if args[n] == '-c' or args[n] == '-charset':
-            charset = args[n+1]
-        elif args[n] == '-s' or args[n] == '-size':
-            size = int( args[n+1] )
-        elif args[n] == '-r' or args[n] == '-ratio':
-            ratio = float( args[n+1] )
+    img = args.image
+    charset = args.charset
+    size = args.size
+    ratio = args.ratio
 
     Characterize().img2chars(img, size, ratio, charset)
+
 
 if __name__ == '__main__':
     main()
